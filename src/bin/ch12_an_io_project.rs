f51dbf7b6552718934d9b0e6b88d5520581e265c
Gardenia <gardenia.wu@outlook.com>
2024-02-02 01:07:00 +0800

    12 An I/O Project: Building a Command Line Program

--- src/bin/ch12_an_io_project.rs
+++ src/bin/ch12_an_io_project.rs
@@ -1,1 +1,20 @@
-fn main() {}
+use rl::{run, Config};
+use std::{env, ffi::OsString, process};
+
+fn main() {
+    let args: Vec<OsString> = env::args_os().collect();
+    dbg!(args);
+
+    let args: Vec<String> = env::args().collect();
+    dbg!(&args);
+
+    let cfg = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = run(&cfg) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::{env, error::Error, f32::consts::E, fs};
+
 mod front_of_house {
     pub mod hosting {
         pub fn add_to_wait_list() {}
@@ -70,7 +72,122 @@ fn internal_adder(a: i32, b: i32) -> i32 {
     a + b
 }
 
-#[test]
-fn internal() {
-    assert_eq!(4, internal_adder(2, 2));
+pub fn run(cfg: &Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(&cfg.file_path)?;
+    dbg!(&contents);
+
+    let results = if cfg.ignore_case {
+        search_case_insensitive(&cfg.query, &contents)
+    } else {
+        search(&cfg.query, &contents)
+    };
+
+    for (len, line) in results.iter().enumerate() {
+        println!("{len}: {line}");
+    }
+    Ok(())
+}
+
+pub struct Config {
+    query: String,
+    file_path: String,
+    pub ignore_case: bool,
+}
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
+    }
+
+    pub fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        let ignore_case = false;
+
+        Config {
+            query,
+            file_path,
+            ignore_case,
+        }
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        } else {
+            dbg!(line);
+        }
+    }
+
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    let query = query.to_lowercase();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        } else {
+            dbg!(line);
+        }
+    }
+
+    results
+}
+#[cfg(test)]
+mod test {
+
+    use super::*;
+
+    #[test]
+    fn internal() {
+        assert_eq!(4, internal_adder(2, 2));
+    }
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
 }
